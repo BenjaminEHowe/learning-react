d58f0a69e324ff4147c6acfcc60b452a83460c27
Benjamin Howe <ben@bh96.uk>
2019-03-29 15:59:52 +0000

    Added basic browserify setup

--- 01-basic-react-flux/gulpfile.js
+++ 01-basic-react-flux/gulpfile.js
@@ -3,12 +3,17 @@
 const gulp = require("gulp");
 const connect = require("gulp-connect"); // runs a local dev server
 const open = require("gulp-open"); // opens a url in a web browser
+const browserify = require("browserify"); // bundles javascript
+const reactify = require("reactify"); // transforms react JSX to JS
+const source = require("vinyl-source-stream"); // use conventional text streams with gulp
 
 const config = {
     "devBaseUrl": "http://127.0.0.1",
     "paths": {
         "dist": "./dist",
         "html": "./src/*.html",
+        "js": "./src/**/*.js",
+        "mainJs": "./src/main.js",
     },
     "port": 9005,
 }
@@ -37,9 +42,21 @@ gulp.task("html", (done) => {
     done();
 });
 
-gulp.task("watch", (done) => {
-    gulp.watch(config.paths.html, gulp.series("html"));
+gulp.task("js", (done) => {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on("error", console.error.bind(console))
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest(`${config.paths.dist}/js`))
+        .pipe(connect.reload());
     done();
 });
 
-gulp.task("default", gulp.series("html", "open", "watch"));
+gulp.task("watch", (done) => {
+    gulp.watch(config.paths.html, gulp.series("html"));
+    gulp.watch(config.paths.js, gulp.series("js"));
+    done();
+});
+
+gulp.task("default", gulp.series("html", "js", "open", "watch"));
