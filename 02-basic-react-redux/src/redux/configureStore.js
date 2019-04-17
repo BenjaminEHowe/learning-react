a6e5ddda24cfe7cf6a9dcaacaff226d51e3146dd
Benjamin Howe <ben@bh96.uk>
2019-04-17 16:50:57 +0100

    Tweaked CoursesPage to display courses from API

--- 02-basic-react-redux/src/redux/configureStore.js
+++ 02-basic-react-redux/src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers/root";
 
 export default function configureStore(initialState) {
-  const addSupportForReduxDevTools =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const addSupportForReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return createStore(
     rootReducer,
     initialState,
-    addSupportForReduxDevTools(applyMiddleware(reduxImmutableStateInvariant()))
+    addSupportForReduxDevTools(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
