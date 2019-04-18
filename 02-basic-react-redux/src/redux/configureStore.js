4ce5878259235c570cabdd5e4a310ed90dd91eef
Benjamin Howe <ben@bh96.uk>
2019-04-18 10:57:08 +0100

    Some cleaning

--- 02-basic-react-redux/src/redux/configureStore.js
+++ 02-basic-react-redux/src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers/root";
+import rootReducer from "./reducers/_root";
 
 export default function configureStore(initialState) {
   const addSupportForReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
